7dc990df116eecf3abd950cb6e334c0def1e19de
Isaac <22546@virtual.utsc.edu.mx>
2026-06-24 05:10:37 -0600

    Agregar ruta controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const { sendMail } = require('../services/mail.service');
 
 const COOKIE_NAME = 'token';
 
