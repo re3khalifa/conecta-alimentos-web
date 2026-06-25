9f26c127c704405db643ac908f5464253ad54bc0
Isaac <22546@virtual.utsc.edu.mx>
2026-06-24 22:25:31 -0600

    Upgrade service

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,14 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  family: 4 // Forzar IPv4
+  }
 });
 
 async function sendMail(to, subject, html) {
