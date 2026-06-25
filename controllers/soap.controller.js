e5954124c761263a0d258e1ee8d088b1bea1b720
Isaac <22546@virtual.utsc.edu.mx>
2026-06-24 23:35:29 -0600

    Up cont soap

--- controllers/soap.controller.js
+++ controllers/soap.controller.js
@@ -1,38 +1,85 @@
+const mongoose = require("mongoose");
 const Offer = require("../models/Offer");
 
 exports.getOffersSOAP = async (req, res, next) => {
   try {
 
-    const offers = await Offer.find().lean();
+    let offers = [];
+
+    // Si envían un id
+    if (req.query.id) {
+
+      if (!mongoose.Types.ObjectId.isValid(req.query.id)) {
+        return res
+          .status(400)
+          .type("text/xml")
+          .send(`<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <Fault>
+      <Code>400</Code>
+      <Message>ID inválido</Message>
+    </Fault>
+  </soap:Body>
+</soap:Envelope>`);
+      }
+
+      const offer = await Offer.findById(req.query.id).lean();
+
+      if (!offer) {
+        return res
+          .status(404)
+          .type("text/xml")
+          .send(`<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <Fault>
+      <Code>404</Code>
+      <Message>Oferta no encontrada</Message>
+    </Fault>
+  </soap:Body>
+</soap:Envelope>`);
+      }
+
+      offers.push(offer);
+
+    } else {
+
+      offers = await Offer.find().lean();
+
+    }
 
     let offersXML = "";
 
     offers.forEach((offer) => {
+
       offersXML += `
-        <Offer>
-          <Id>${offer._id}</Id>
-          <Title>${offer.title}</Title>
-          <Category>${offer.category}</Category>
-          <Price>${offer.price}</Price>
-          <Quantity>${offer.quantity}</Quantity>
-          <ExpirationDate>${offer.expirationDate}</ExpirationDate>
-        </Offer>
+      <Offer>
+        <Id>${offer._id}</Id>
+        <Title>${offer.title}</Title>
+        <Description>${offer.description}</Description>
+        <Category>${offer.category}</Category>
+        <Price>${offer.price}</Price>
+        <Quantity>${offer.quantity}</Quantity>
+        <ExpirationDate>${offer.expirationDate}</ExpirationDate>
+      </Offer>
       `;
+
     });
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 
 <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
 
-  <soap:Body>
+<soap:Body>
 
-    <GetOffersResponse>
+<GetOffersResponse>
 
-      ${offersXML}
+${offersXML}
 
-    </GetOffersResponse>
+</GetOffersResponse>
 
-  </soap:Body>
+</soap:Body>
 
 </soap:Envelope>`;
 
