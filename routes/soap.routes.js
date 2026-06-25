88969d1a5459e30105b19a1fc9c4d2453474d3bc
Isaac <22546@virtual.utsc.edu.mx>
2026-06-25 00:19:15 -0600

    Up soaproutes

--- routes/soap.routes.js
+++ routes/soap.routes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const path = require("path");
 
 const soapController = require("../controllers/soap.controller");
 
 router.get("/", soapController.getOffersSOAP);
+
+router.get("/wsdl", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "public", "wsdl", "ofertas.wsdl")
+  );
+});
 
 module.exports = router;
